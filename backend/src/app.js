dc7ff2f0bfffef7f228e1e2490f2c651e38aca93
ritam413 <ritamm413@gmail.com>
2025-08-01 18:54:07 +0530

    fixing cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 const app = express()
 
-const allowedOrigins = process.env.CORS_ORIGIN
+const allowedOrigins = process.env.CORS_ORIGIN.split(",");
 
 app.use(cors({
     origin: function (origin, callback) {
